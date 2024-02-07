98d8414fed96ff7c0e6663e672e48999093ced05
ahmedsaadbadawy <ahmedzack168@gmail.com>
2024-02-07 17:48:52 +0200

    firebase added

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,14 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
+import 'firebase_options.dart';
 import 'views/login_screen.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const FastBuy());
 }
 
@@ -20,4 +26,3 @@ class FastBuy extends StatelessWidget {
     );
   }
 }
-
